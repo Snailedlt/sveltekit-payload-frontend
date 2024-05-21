231766bbf172ce1751bca00146e63e3671dbe770
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2024-05-21 18:57:09 +0200

    first commit

---
+++ src/lib/payload/collections/User.ts
@@ -0,0 +1,19 @@
+import type { CollectionConfig } from 'payload/types';
+
+export const Users: CollectionConfig = {
+	slug: 'users',
+	admin: {
+		useAsTitle: 'email'
+	},
+	auth: true,
+	fields: [
+		// Email added by default
+		// Add more fields as needed
+	],
+	access: {
+		create: () => false,
+		delete: () => false,
+		update: () => false,
+		read: () => true
+	}
+};
